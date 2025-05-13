55684e8c9776c972e7d99d9dbdd9c4db0d9d0925
Melih Kale <59390060+melihkl@users.noreply.github.com>
2025-05-13 08:15:16 +0300

    Add update, delete, and exit functionalities to CLI task tracker

--- 01-task-tracker-cli/main.go
+++ 01-task-tracker-cli/main.go
@@ -4,32 +4,94 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"task/task"
 )
 
-
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("\n--- GÖREV TAKİP ---")
+		fmt.Println("1. Görev Ekle")
+		fmt.Println("2. Görevleri Listele")
+		fmt.Println("3. Görev Sil")
+		fmt.Println("4. Görev Güncelle")
+		fmt.Println("5. Çıkış Yap")
+		fmt.Print("Seçiminiz: ")
 
-	fmt.Print("Başlık: ")
-	title, _ := reader.ReadString('\n')
+		secim, _ := reader.ReadString('\n')
+		secim = strings.TrimSpace(secim)
 
-	fmt.Print("Açıklama: ")
-	description, _ := reader.ReadString('\n')
+		switch secim {
+		case "1":
+			fmt.Print("Başlık: ")
+			title, _ := reader.ReadString('\n')
 
-	fmt.Print("Durum: ")
-	status, _ := reader.ReadString('\n')
+			fmt.Print("Açıklama: ")
+			description, _ := reader.ReadString('\n')
 
-	title = strings.TrimSpace(title)
-	description = strings.TrimSpace(description)
-	status = strings.TrimSpace(status)
+			fmt.Print("Durum: ")
+			status, _ := reader.ReadString('\n')
 
-	// Add the task
-	task.AddTask(title, description, status)
+			title = strings.TrimSpace(title)
+			description = strings.TrimSpace(description)
+			status = strings.TrimSpace(status)
 
-	// List all tasks
-	task.ListTask()
+			task.AddTask(title, description, status)
+
+		case "2":
+			task.ListTask()
+
+		case "3":
+			fmt.Print("Silmek için görev ID giriniz\n")
+			idInput, _ := reader.ReadString('\n')
+			id, err := strconv.Atoi(strings.TrimSpace(idInput))
+
+			if err != nil {
+				fmt.Printf("Bu ID için görev kaydı bulunmamaktadır!")
+				continue
+			}
+
+			if !task.Exists(id) {
+				fmt.Printf("ID %d ile görev bulunamadı.\n", id)
+				continue
+			}
+			task.DeleteTask(id)
+
+		case "4":
+			fmt.Print("Görev ID giriniz : ")
+			idInput, _ := reader.ReadString('\n')
+			id, err := strconv.Atoi(strings.TrimSpace(idInput))
+
+			if err != nil {
+				fmt.Printf("Bu ID için görev kaydı bulunmamaktadır!")
+				continue
+			}
+
+			if !task.Exists(id) {
+				fmt.Printf("ID %d ile görev bulunamadı.\n", id)
+				continue
+			}
+
+			fmt.Print("Güncel Başlık: ")
+			title, _ := reader.ReadString('\n')
+
+			fmt.Print("Güncel Açıklama: ")
+			description, _ := reader.ReadString('\n')
+
+			fmt.Print("Güncel Durum: ")
+			status, _ := reader.ReadString('\n')
+
+			title = strings.TrimSpace(title)
+			description = strings.TrimSpace(description)
+			status = strings.TrimSpace(status)
+
+			task.UpdateTask(id, title, description, status)
+
+		case "5":
+			fmt.Printf("Çıkış yaptınız!")
+			return
+		}
+	}
 }
-
