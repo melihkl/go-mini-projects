0f3b3042c6a11cdf652ae8fe9ecbd720542aa91a
Melih Kale <59390060+melihkl@users.noreply.github.com>
2025-05-13 08:15:42 +0300

    Add update, delete, and exit functionalities to CLI task tracker

--- 01-task-tracker-cli/task/task.go
+++ 01-task-tracker-cli/task/task.go
@@ -11,7 +11,6 @@ type Task struct {
 	Status      string
 }
 
-
 var tasks []Task
 
 func AddTask(title, description, status string) {
@@ -34,4 +33,50 @@ func ListTask() {
 	for _, task := range tasks {
 		fmt.Printf("ID: %d, Title: %s, Description: %s, Status: %s\n", task.ID, task.Title, task.Description, task.Status)
 	}
+}
+
+func DeleteTask(id int) {
+	index := -1
+
+	for i, task := range tasks {
+		if task.ID == id {
+			index = i
+			break
+		}
+	}
+
+	tasks = append(tasks[:index], tasks[index+1:]...)
+	fmt.Printf("ID %d nolu görev silindi\n", id)
+
+	if index == -1 {
+		fmt.Printf("ID %d nolu görev bulunamadı\n", id)
+		return
+	}
+}
+
+func UpdateTask(id int, title, description, status string) {
+	found := false
+	for i := range tasks {
+		if tasks[i].ID == id {
+			found = true
+			tasks[i].Title = title
+			tasks[i].Description = description
+			tasks[i].Status = status
+			fmt.Printf("ID %d nolu görev güncellendi\n", id)
+			return
+		}
+	}
+
+	if !found {
+		fmt.Printf("ID %d nolu görev bulunmamaktadır\n", id)
+	}
+}
+
+func Exists(id int) bool {
+	for _, t := range tasks {
+		if t.ID == id {
+			return true
+		}
+	}
+	return false
 }
