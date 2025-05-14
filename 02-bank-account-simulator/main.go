f79df63211fbd19035f8be4cab8f7e940c5cfda2
Melih Kale <59390060+melihkl@users.noreply.github.com>
2025-05-14 08:06:08 +0300

    Update main.go

--- 02-bank-account-simulator/main.go
+++ 02-bank-account-simulator/main.go
@@ -1,1 +1,49 @@
+package main
 
+import (
+	"account/account"
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	islem := flag.String("islem", "", "Yapilacak islem: yatir, cek, toplam bakiye")
+	miktar := flag.Float64("miktar", 0, "yatirilacak ya da cekilecek miktar")
+	kisi := flag.String("kisi", "", "hesap sahibi")
+
+	flag.Parse()
+
+	//hesap olusturma islemi
+
+	hesap := account.Hesap{Kisi: *kisi, Bakiye: 0.0}
+
+	switch *islem {
+	case "yatir":
+		if *miktar <= 0 {
+			fmt.Printf("yatirilacak miktar pozitif olmali")
+			return
+		}
+
+		err := hesap.Yatir(*miktar)
+
+		if err != nil {
+			fmt.Println("Hata", err)
+		}
+
+	case "cek":
+		if *miktar <= 0 {
+			fmt.Printf("cekilecek miktar pozitif olmali")
+			return
+		}
+
+		err := hesap.Cek(*miktar)
+
+		if err != nil {
+			fmt.Println("Hata", err)
+		}
+	case "toplamBakiye":
+		hesap.ToplamBakiye()
+	}
+
+}
